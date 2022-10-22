31d29395c581c70d9888995423e087106e8db023
wjdanfu <wjdanfu@gmail.com>
2022-10-22 23:13:06 +0900

    패스워드 암호화

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.domain.UserRepository;
 import com.example.demo.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -10,13 +11,18 @@ import java.util.UUID;
 @Service
 public class UserServiceImpl implements UserService{
 
-    @Autowired
-    UserRepository userRepository;
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository,BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public void createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
-        userDto.setEncryptedPwd("tmp password");
+        userDto.setEncryptedPwd(passwordEncoder.encode(userDto.getPassword()));
         userRepository.save(userDto.toEntity());
     }
 }
